2ab8f3677b8efc1120ea422e39ebed38ae216cd5
shafanaura <shafanaura48@gmail.com>
2020-12-23 21:24:22 +0700

    second commit

--- task4.js
+++ task4.js
@@ -1,8 +1,15 @@
 const fetch = require("node-fetch");
 
 const getData = async () => {
-	const data = await fetch("https://jsonplaceholder.typicode.com/users");
-	console.log(await data.json());
+	fetch(`https://jsonplaceholder.typicode.com/users`)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			data.map((person) => {
+				console.log(person.name);
+			});
+		});
 };
 
 getData();
